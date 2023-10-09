c4421a3044ab945b8e1726bcc5733f9beb1ca93b
Joe Fuentes <job@joefuentes.com>
2023-10-08 18:25:20 -0700

    Update search
    Update UI so that if its the same ZIP then show reload icon (added)
    update styles - orange select

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import { Header } from './Header';
 import { AirPollution } from './AirPollution';
@@ -16,7 +16,9 @@ export const WeatherData = () => {
     const [temperature, setTemperature] = useState([]);
     const [zipNotFound, setZipNotFound] = useState({ zip: '', notFound: false })
     const [locationState, setLocationState] = useState('')
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [, updateState] = useState();
+    const reset = useCallback(() => updateState({}), []);
 
     const clearAll = () => {
         setLocationCity(null)
@@ -26,54 +28,89 @@ export const WeatherData = () => {
         setZip(null)
         setLocationState(null)
     }
-
-    useEffect(() => {
-        if (zip) {
-
-            const latiudeAndLongitudeURL = `https://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`
-            fetch(latiudeAndLongitudeURL).then(resp => resp.json()).then(data => {
-                if (data.cod) {
-                    if (data.cod === '404') setZipNotFound({ zip, notFound: true })
-                    clearAll();
-                } else {
-
-                    const stateURL = `https://zip-api.eu/api/v1/info/US-${zip}?fields=state`;
-                    fetch(stateURL).then(resp => resp.json())
-                        .then(state => {
-                            console.log({ state })
-                            setLocationState(state.error ? '' : state)
+    /*
+        useEffect(() => {
+            if (zip) {
+    
+                const latiudeAndLongitudeURL = `https://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`
+                fetch(latiudeAndLongitudeURL).then(resp => resp.json()).then(data => {
+                    if (data.cod) {
+                        if (data.cod === '404') setZipNotFound({ zip, notFound: true })
+                        clearAll();
+                    } else {
+    
+                        const stateURL = `https://zip-api.eu/api/v1/info/US-${zip}?fields=state`;
+                        fetch(stateURL).then(resp => resp.json()).then(state => setLocationState(state.error ? '' : state))
+    
+                        setZipNotFound({ zip, notFound: false })
+                        setLocationCity(data);
+                        const { lat, lon } = data;
+                        const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`
+                        fetch(weatherURL).then(resp => resp.json()).then(data => {
+                            const { sunset, sunrise } = data.sys;
+                            setSunData({ sunset, sunrise })
+                            const { temp, feels_like, temp_max, temp_min } = data.main
+                            setTemperature([temp, feels_like, temp_max, temp_min])
+                            setIsLoading(false)
                         })
-                        .catch(err => console.log({ err }))
+    
+                        const polutionURL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`;
+                        fetch(polutionURL).then(resp => resp.json()).then(data => {
+                            const { components } = data.list[0];
+                            const airQualityComponents = []
+                            Object.entries(components).forEach((k, index) => {
+                                const value = { id: index + 1, label: k[0], qty: k[1] }
+                                airQualityComponents.push(value)
+                            })
+    
+                            setAirQuality(airQualityComponents)
+                        })
+                    }
+    
+                }).catch(err => console.log(err));
+            }
+    
+        }, [zip]);
+       */
+    const fetchWeatherData = (zip) => {
+        console.log({ zip })
+        const latiudeAndLongitudeURL = `https://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`
+        fetch(latiudeAndLongitudeURL).then(resp => resp.json()).then(data => {
+            if (data.cod) {
+                if (data.cod === '404') setZipNotFound({ zip, notFound: true })
+                clearAll();
+            } else {
 
-                    setZipNotFound({ zip, notFound: false })
-                    setLocationCity(data);
-                    const { lat, lon } = data;
-                    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`
-                    fetch(weatherURL).then(resp => resp.json()).then(data => {
-                        const { sunset, sunrise } = data.sys;
-                        setSunData({ sunset, sunrise })
-                        const { temp, feels_like, temp_max, temp_min } = data.main
-                        setTemperature([temp, feels_like, temp_max, temp_min])
-                        setIsLoading(false)
+                const stateURL = `https://zip-api.eu/api/v1/info/US-${zip}?fields=state`;
+                fetch(stateURL).then(resp => resp.json()).then(state => setLocationState(state.error ? '' : state))
+
+                setZipNotFound({ zip, notFound: false })
+                setLocationCity(data);
+                const { lat, lon } = data;
+                const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`
+                fetch(weatherURL).then(resp => resp.json()).then(data => {
+                    const { sunset, sunrise } = data.sys;
+                    setSunData({ sunset, sunrise })
+                    const { temp, feels_like, temp_max, temp_min } = data.main
+                    setTemperature([temp, feels_like, temp_max, temp_min])
+                    setIsLoading(false)
+                })
+
+                const polutionURL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`;
+                fetch(polutionURL).then(resp => resp.json()).then(data => {
+                    const { components } = data.list[0];
+                    const airQualityComponents = []
+                    Object.entries(components).forEach((k, index) => {
+                        const value = { id: index + 1, label: k[0], qty: k[1] }
+                        airQualityComponents.push(value)
                     })
 
-                    const polutionURL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=df1ad6d37d106029c2cec1eeeb6c13a1`;
-                    fetch(polutionURL).then(resp => resp.json()).then(data => {
-                        const { components } = data.list[0];
-                        const airQualityComponents = []
-                        Object.entries(components).forEach((k, index) => {
-                            const value = { id: index + 1, label: k[0], qty: k[1] }
-                            airQualityComponents.push(value)
-                        })
+                    setAirQuality(airQualityComponents)
+                })
+            }
 
-                        setAirQuality(airQualityComponents)
-                    })
-                }
-
-            }).catch(err => console.log(err));
-        }
-
-    }, [zip]);
+        }).catch(err => console.log(err));
+    }
 
     const setInput = e => setInputData(e.target.value);
     const setSearch = () => submit();
@@ -82,17 +119,18 @@ export const WeatherData = () => {
     }
 
     const submit = () => {
-        setZipNotFound(false)
-        setZip(inputData)
-        setIsLoading(true)
+        if (inputData !== zip) {
+            setZipNotFound(false)
+            setZip(inputData)
+            setIsLoading(true)
+        }
+        fetchWeatherData(inputData)
     }
-
-    console.log({ locationCity, locationState })
 
     return (
         <div>
-            <input type="text" onChange={setInput} onKeyDown={handleEnter} placeholder="Enter a Zipcode" />
-            <button onClick={setSearch}>Search</button>
+            <input type="text" onChange={setInput} value={inputData} onKeyDown={handleEnter} placeholder="Enter a Zipcode" />
+            <button onClick={setSearch}>{inputData === zip ? <img src='reload-icon-16900.png' alt='refresh' /> : 'Search'}</button>
 
             {isLoading && <Loading />}
             {zip && !isLoading &&
--- src/shared/utils.js
+++ src/shared/utils.js
@@ -3,6 +3,6 @@ export const getDateFromSeconds = seconds => {
     return date.toLocaleTimeString()
 }
 
-export const getFarinhaitTemp = (kelvin) => ((kelvin - 273.15) * 1.8 + 32).toFixed(2);
+export const getFarinhaitTemp = (kelvin) => ((kelvin - 273.15) * 1.8 + 32).toFixed(2) + ' F°';
 
-export const getCelciusTemp = (kelvin) => (kelvin - 273.15).toFixed(2);
+export const getCelciusTemp = (kelvin) => (kelvin - 273.15).toFixed(2) + ' C°';
